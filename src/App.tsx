19fe30f05a2188706cc6338b1a3455e878550496
Gurvan <131682444+GurvanENess@users.noreply.github.com>
2025-05-15 16:01:38 +0200

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import { ChatProvider } from './contexts/ChatContext';
 import ProtectedRoute from './lib/ProtectedRoute';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
@@ -10,25 +9,23 @@ import Home from './pages/Home';
 function App() {
   return (
     <AuthProvider>
-      <ChatProvider>
-        <Router>
-          <div className="min-h-screen bg-gray-100">
-            <Navbar />
-            <Routes>
-              <Route path="/login" element={<Login />} />
+      <Router>
+        <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
             
-              {/* Protected routes */}
-              <Route element={<ProtectedRoute />}>
-                <Route path="/" element={<Home />} />
-                {/* Add more protected routes here */}
-              </Route>
+            {/* Protected routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Home />} />
+              {/* Add more protected routes here */}
+            </Route>
             
-              {/* Redirect to home for any other routes */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </div>
-        </Router>
-      </ChatProvider>
+            {/* Redirect to home for any other routes */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
