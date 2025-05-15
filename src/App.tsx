d083d08210a49bd444085591e182bcf99f28aa64
Gurvan <131682444+GurvanENess@users.noreply.github.com>
2025-05-15 16:05:39 +0200

    Implement Enterprise Chat Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { Toaster } from 'react-hot-toast';
-import ChatLayout from './components/ChatLayout';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './lib/ProtectedRoute';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Provider store={store}>
-      <ChatLayout />
-      <Toaster position="top-right" />
-    </Provider>
+    <AuthProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-100">
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            
+            {/* Protected routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Home />} />
+              {/* Add more protected routes here */}
+            </Route>
+            
+            {/* Redirect to home for any other routes */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
